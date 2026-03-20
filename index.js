8213b07d29df25e7be3636f1374c8fee252d65af
ArnavHD <arnavpaul180@gmail.com>
2026-03-20 01:17:23 -0600

    Finally done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
 
